4cba25cd5b608149d849488c5a282cd8011a46b1
Alejandra Olivo <alejandraolivo.student@careerdevs.com>
2018-10-29 23:23:55 +0000

    Should probably add an if/else statement at some point

--- hotelObject.js
+++ hotelObject.js
@@ -1,13 +1,32 @@
 var readline = require('readline-sync');
-var name = readline.question("Welcome to Welcome to the LUX Hotel, what is your name? ");
-console.log("It's lovely to meet you, " + name + " !");
+var guest = readline.question("Welcome to the LUX Hotel, what is your name? ");
+console.log("It's lovely to meet you, " + guest + " !");
 
 var luxHotel = {
-    room1: "The Wedding Suite",
-    room2: "The Platinum Suite",
-    room3: "The Princess Suite",
-    room4: "The Diamond Suite",
-    amenities: ["hotTub", "pool", "gym", "sauna"]
+    "The Wedding Suite" : {
+    room: 1,
+    name: "The Wedding Suite",
+    price: 120,
+    available: true,
+},
+    "The Platinum Suite" : {
+    room: 2,
+    name: "The Platinum Suite",
+    price:220,
+    available: true,
+},
+    "The Princess Suite" : {
+    room: 3,
+    name: "The Princess Suite",
+    price: 150,
+    available: true,
+},
+"The Diamond Suite" : {
+    room: 4,
+    name: "The Diamond Suite",
+    price: 200,
+    available: true,
+}
 };
 
 var yes = readline.question("Would you like to see what suites we have available? ");
@@ -16,8 +35,9 @@ var roomArray = ["The Wedding Suite", "The Platinum Suite", "The Princess Suite"
 for (var i = 0; i < roomArray.length; i++)
 console.log(roomArray[i]);
 
-var amenities = ["HotTub", "Pool", "Gym", "Sauna", "Room-Service"]
-console.log("And, of course, there are our amenities: " + amenities)
+var amenities = ["HotTub", " Pool", " Gym", " Sauna", " Room-Service"];
+console.log("And, of course, there are our amenities: " + amenities);
 
-var roomChoice = readline.question("Which one of rooms would you be interested in booking? ")
+var roomChoice = readline.question("Which one of rooms would you be interested in booking? ");
 //instead of using an object with arrays inside of it, use arrays with objects inside of it
+console.log("That's a great choice, " + guest + ", " + luxHotel[roomChoice].name + " is " + luxHotel[roomChoice].price  + " per night. ");
---
+++ joshExample.js
@@ -0,0 +1,65 @@
+function rentRoom() {
+    var y = "Y";
+    var n = "N";
+    var readline = require('readline-sync');
+    //This is where they will be told which rooms are available if they want that is...
+    var yesOrNo = readline.question("Welcome to the Chance Hotel! We have 4 rooms avaiable. Would you like to rent one? Y/N:").toUpperCase();
+    //This is the hotel object. This has the list of rooms and amenities
+    var vacantRooms = {
+        room1: "The Classy Room",
+        room2: "The Nightmare Room",
+        room3: "The Gold room",
+        room4: "The Trashy Room",
+    };
+    var occupiedRooms = [];
+    var amenities = [
+        [" Tea Time every 2 hours", " Classical Music will be played", " You have the finest silverware", " and Ludwig van Beethoven will come into your room from time to time! "],
+        [" Freddy Kreuger will visit your dreams", " you'll be rooming with Satan", " The floor is lava", " and it hails brimstone inside at all times! "],
+        [" Literally Everything in your room is made out of gold", " Theres a gold pooping goose", " Rumpelstiltskin is spinning gold in the corner", " and it will periodically rain gold! "],
+        [" You'll be rooming with Oscar the grouch", " The floor will be covered in bile", " Your bed is a cardboard mat", " and it rains ketchup every now and then... "]
+    ];
+    //If they say y", then it will list the rooms
+    if (yesOrNo == y.toUpperCase()) {
+        var rooms = Object.keys(vacantRooms);
+        for (var singleroom of rooms) {
+            console.log(singleroom);
+        }
+        var whichRoom = readline.question("Above are our avaiable rooms. Which one would you like to rent? '1,2,3, or 4?'")
+        // If they choose room 1, then it will tell them the amenities and the remaining vacant rooms.
+        if (whichRoom == 1) {
+            occupiedRooms.push(rooms[0]);
+            delete rooms[0];
+            console.log("Congratualtions! You've chosen the 'Classy' Room! These are your amenities:" + amenities[1] + "Enjoy your stay!");
+            console.log("The rooms left available are" + rooms + ". Tell a friend to test their luck at the Chance hotel!");
+        }
+        else if (whichRoom == 2) {
+            // If they choose room 2, then it will tell them the amenities and the remaining vacant rooms.
+            occupiedRooms.push(rooms[1]);
+            delete rooms[1];
+            console.log("Congratualtions! You've chosen the 'Nightmare' Room! These are your amenities:" + amenities[2] + "Enjoy your stay!")
+            console.log("The rooms left available are" + rooms + ". Tell a friend to test their luck at the Chance hotel!");
+        }
+        else if (whichRoom == 3) {
+            // If they choose room 3, then it will tell them the amenities and the remaining vacant rooms.
+            occupiedRooms.push(rooms[2]);
+            delete rooms[2];
+            console.log("Congratualtions! You've chosen the 'Gold' Room! These are your amenities:" + amenities[3] + "Enjoy your stay!")
+            console.log("The rooms left available are" + rooms + ". Tell a friend to test their luck at the Chance hotel!");
+        }
+        else if (whichRoom == 4) {
+            // If they choose room 4, then it will tell them the amenities and the remaining vacant rooms.
+            occupiedRooms.push(rooms[3]);
+            delete rooms[3];
+            console.log("Congratualtions! You've chosen the 'Trashy' Room! These are your amenities:" + amenities[4] + "Enjoy your stay!")
+            console.log("The rooms left available are " + rooms + ". Tell a friend to test their luck at the Chance hotel!");
+        }
+    }
+    //If they say "n", then it will say thanks for coming
+    else if (yesOrNo == n.toUpperCase()) {
+        console.log("Great choice. Find someplace safer to stay... Have a great day!");
+        return;
+    }
+    return;
+}
+
+return rentRoom();
