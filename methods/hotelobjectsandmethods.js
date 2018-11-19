3c4373690191c17cbca1e7c52e743255c45316ee
Alejandra Olivo <alejandraolivo.student@careerdevs.com>
2018-11-19 01:10:36 +0000

    still working on solving the booking problem

--- methods/hotelobjectsandmethods.js
+++ methods/hotelobjectsandmethods.js
@@ -20,11 +20,20 @@ var hotel = {
         return this.roomNumbersAvailable.length + this.roomNumbersBooked.length;
     },
     
-    bookRoom: function() {
+    bookRoom: function(position) {
         //only book a room if one or more are available
         if (this.roomNumbersAvailable > 0) {
             //remove the booked room from roomNumbersAvailable and add it to roomNumbersBooked
             PRACTICE THIS ON YOUR OWN
+            this.roomNumbersAvailable.splice(position, 1)
+            var removedItem = this.roomNumbersAvailable.splice(position, 1)
+            this.roomNumbersBooked.push(removedItem)
+            
+        } else {
+
+        }
+            
+            
         }
     }
 };
@@ -38,4 +47,4 @@ ar.pop(); // returns 6
 console.log( ar ); // [1, 2, 3, 4, 5]
 
 
-var removedItem = ar.pop()
+var removedPop = ar.pop()
