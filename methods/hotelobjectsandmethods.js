fd688d65b3fc13808e7d0cdf3da87b9ea82d0281
Alejandra Olivo <alejandraolivo.student@careerdevs.com>
2018-11-21 00:33:15 +0000

    worked in class

--- methods/hotelobjectsandmethods.js
+++ methods/hotelobjectsandmethods.js
@@ -24,18 +24,20 @@ var hotel = {
         //only book a room if one or more are available
         if (this.numberOfRoomsAvailable() > 0) {
             //remove the booked room from roomNumbersAvailable and add it to roomNumbersBooked
-             var randomRoom = this.roomNumbersAvailable[Math.floor(Math.random()*this.roomNumbersAvailable.length)]
+            //the Math. stuff makes your selection random 
+             var randomRoom = this.roomNumbersAvailable[Math.floor(Math.random()*this.roomNumbersAvailable.length)];
             //this.roomNumbersAvailable.splice(this.roomNumbersAvailable.indexOf(randomRoom), 1);
             this.roomNumbersAvailable.splice(this.roomNumbersAvailable.indexOf(randomRoom), 1);
            //add number to booked room
            //this.roomNumbersBooked.push(randomRoom);
-           //this.roomNumbersBooked = this.roomNumbersBooked.concat(randomRoom);
-           
-           this.roomNumbersBooked = this.roomNumbersBooked.unshift(randomRoom);
-           console.log(this.roomNumbersBooked)
+           this.roomNumbersBooked = this.roomNumbersBooked.concat(randomRoom);
+           //this.roomNumbersBooked = this.roomNumbers.splice(this.roomNumbers.indexOf(randomRoom), 1).concat(this.roomNumbersBooked);
+           //this.roomNumbersBooked = this.roomNumbersAvailable.splice(this.roomNumbersAvailable.indexOf(randomRoom), 1);
+           console.log(this.roomNumbersBooked);
         }
     }
 };
 
+//Math.floor
 //couldnt make this work. it takes a room out of this.roomNumbersAvailable and seems to insert a position into this.roomNumbersBooked
 //but it doesnt insert the actual string and then it wont let me use the bookRoom() method more than once
